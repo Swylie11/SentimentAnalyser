e31068ecf02c5b384d2b6b369f25605e9e41d915
Swylie11 <sam.james.wylie@gmail.com>
2024-11-08 15:50:07 +0000

    Allowed for a custom batch size

--- Spike1/main.py
+++ Spike1/main.py
@@ -1,17 +1,17 @@
 import WordVectorConversions as wvc
 import json
 from types import SimpleNamespace
-
+import time
 # entry_data = input('Please input the text to be analysed: ')
 
 
-def fetch_test_data(test_data):
+def fetch_test_data(test_data, batch_size):
     """ Fetches batch review data from the test data file """
     count = 0
     with open(test_data, 'r', encoding='utf8') as f:
         output = []
         for line in f:  # For every review
-            if count < 10:
+            if count < batch_size:
                 review = json.loads(line, strict=False, object_hook=lambda d: SimpleNamespace(**d))
                 output.append(review.text)  # Adds the review to the batch output
                 count += 1
@@ -21,10 +21,16 @@ def fetch_test_data(test_data):
         return output
 
 
+batch = int(input("Enter batch size: "))  # Lets the user control the batch size
 test_data_file = 'C:/Users/samja/Documents/SchoolWork/ComputerScience/Project/SentimentAnalyser/Data/TestData.jsonl'
 
-entry_data = fetch_test_data(test_data_file)
+entry_data = fetch_test_data(test_data_file, batch)
 
 print(entry_data)
 print(len(entry_data))
-# print(wvc.pad_matrix(wvc.return_vector_matrix(wvc.format_entry_data(entry_data))))
+
+start = time.time()
+conv_input_data = wvc.pad_matrix(wvc.return_vector_matrix_jsonl(wvc.format_entry_data(entry_data)))
+print(conv_input_data)
+end = time.time()
+print(f"Time elapsed: {end-start}")
